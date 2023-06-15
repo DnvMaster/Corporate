c1ffbad1c866143aa2fc1c3cc04f60fb9838d387
DnvMaster <dnvcomp@yandex.ru>
2023-06-15 11:10:40 +0300

    create table in database

---
+++ database/migrations/2023_06_14_065106_change_portfolios_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangePortfoliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('portfolios', function (Blueprint $table) {
+            $table->foreignId('filter_alias')->constrained('filters');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('portfolios', function (Blueprint $table) {
+            //
+        });
+    }
+}
