1c679833b34e370303c22a1b101e5f9f4fd8a9dc
Mikolai Dziamko <dnvcomp@yandex.ru>
2026-02-04 12:11:25 +0300

    Pagination elements

--- resources/views/corporate/content.blade.php
+++ resources/views/corporate/content.blade.php
@@ -58,22 +58,6 @@
 	</div>
 	@else
 	<div id="content-home" class="content group">
-		<div class="hentry group">
-			<div class="section portfolio">
-				<h3 class="title">{{ __('Articles_no') }}</h3>
-				<div class="hentry work group portfolio-sticky portfolio-full-description">
-				    <div class="work-thumbnail">
-				        <a class="thumb">
-							<img src="{{ asset('corporate/images/home-ix/00313.jpg') }}" alt="{{ __('Articles_no') }}" title="{{ __('Articles_no') }}">
-						</a>
-					</div>
-				</div>
-
-				<div class="work-description">
-				    <p>{{ __('articles_appear_here') }}</p>
-					<a href="{{ url('/') }}" class="read-more">{{ __('return_to_home')}}</a>
-				</div>
-			</div>
-		</div>
+		@include('corporate.articles_no')
 	</div>
 @endif
