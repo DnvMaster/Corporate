5c484a9b802a2b688be01b285f8660cd156396a9
Mikolai Dziamko <dnvcomp@yandex.ru>
2026-02-15 13:50:17 +0300

    Blog & Comments

--- resources/views/corporate/article_content.blade.php
+++ resources/views/corporate/article_content.blade.php
@@ -17,7 +17,7 @@
 			<div class="meta group">
 				<p class="author"><span>{{ __('by') }}&nbsp;<a href="#" title="{{ $article->title }}" rel="author">{{ $article->user->name }}</a></span></p>
 				<p class="categories"><span>{{ __('In') }}:&nbsp;<a href="{{ route('articles.category',['alias'=>$article->category->title]) }}" title="{{ __('View all post in') }}&nbsp;{{ $article->category->title }}" rel="category tag">{{ $article->category->title }}</a></span></p>
-				<p class="comments"><span><a href="#comments" title="Comment on This is the title of the first article. Enjoy it.">{{ count($article->comments) ? count($article->comments) : 0 }} {{ trans_choice('No comments',count($article->comments)) }}</a></span></p>
+				<p class="comments"><span><a href="#comments" title="{{ __('Comment on article') }}">{{ count($article->comments) ? count($article->comments) : 0 }} {{ trans_choice('No comments',count($article->comments)) }}</a></span></p>
 			</div>
 			<!-- post content -->
 			<div class="the-content single group">
@@ -31,68 +31,22 @@
 				    <a href="http://yourinspirationtheme.com/demo/pinkrio/2012/09/24/this-is-the-title-of-the-first-article-enjoy-it/" class="socials-small bookmark-small" title="This is the title of the first article. Enjoy it.">bookmark</a>
 				</div>
 			</div>
-			<p class="tags">Tags: <a href="#" rel="tag">book</a>, <a href="#" rel="tag">css</a>, <a href="#" rel="tag">design</a>, <a href="#" rel="tag">inspiration</a></p>
+			<p class="tags">{{ __('Tags') }}: <a href="#" rel="tag">book</a>, <a href="#" rel="tag">css</a>, <a href="#" rel="tag">design</a>, <a href="#" rel="tag">inspiration</a></p>
 			<div class="clear"></div>
 		</div>
         @endif
 		<!-- START COMMENTS -->
 		<div id="comments">
-			<h3 id="comments-title"><span>2</span> comments </h3>
-			<ol class="commentlist group">
-				<li class="comment even depth-1">
-				    <div class="comment-container">
-				        <div class="comment-author vcard">
-				            <img alt="" src="images/avatar/unknow.png" class="avatar" height="75" width="75">
-                            <cite class="fn">Anonymous</cite>
-				        </div>
-				        <!-- .comment-author .vcard -->
-				        <div class="comment-meta commentmetadata">
-				            <div class="intro">
-				                <div class="commentDate">
-				                    <a href="#comment-2">September 24, 2012 at 1:31 pm</a>
-				                </div>
-				                <div class="commentNumber">#&nbsp;1</div>
-				            </div>
-				            <div class="comment-body">
-				                <p>Hi all, i’m a guest and this is the guest’s awesome comments template!</p>
-				            </div>
-				            <div class="reply group">
-				                <a class="comment-reply-link" href="#respond" onclick="return addComment.moveForm(&quot;comment-2&quot;, &quot;2&quot;, &quot;respond&quot;, &quot;41&quot;)">Reply</a>
-				            </div>
-				            <!-- .reply -->
-				        </div>
-				        <!-- .comment-meta .commentmetadata -->
-				    </div>
-				    <!-- #comment-##  -->
-				</li>
-				<li class="comment bypostauthor odd">
-				    <div class="comment-container">
-				        <div class="comment-author vcard">
-				            <img alt="" src="images/avatar/nicola.jpeg" class="avatar" height="75" width="75" />
-                            <cite class="fn">nicola</cite>
-				        </div>
-				        <!-- .comment-author .vcard -->
-				        <div class="comment-meta commentmetadata">
-				            <div class="intro">
-				                <div class="commentDate">
-				                                        <a href="#">
-				                                        September 24, 2012 at 1:32 pm</a>
-				                                    </div>
-				                                    <div class="commentNumber">#&nbsp;2</div>
-				                                </div>
-				                                <div class="comment-body">
-				                                    <p>While i’m the author of the post. My comment template is different, something like a “sticky comment”!</p>
-				                                </div>
-				                                <div class="reply group">
-				                                    <a class="comment-reply-link" href="#respond" onclick="return addComment.moveForm(&quot;comment-3&quot;, &quot;3&quot;, &quot;respond&quot;, &quot;41&quot;)">Reply</a>
-				                                </div>
-				                                <!-- .reply -->
-				                            </div>
-				                            <!-- .comment-meta .commentmetadata -->
-				                        </div>
-				                        <!-- #comment-##  -->
-				                    </li>
-				                </ol>
+			<h3 id="comments-title"><span>{{ count($article->comments) ? count($article->comments) : 0 }}&nbsp;</span>{{ trans_choice('No comments',count($article->comments)) }}</h3>
+			@settings($comment, $article->comments->groupBy('parent_id'))
+            <ol class="commentlist group">
+                @foreach($comment as $key => $comments)
+                    @if($key !== 0)
+                        @break
+                    @endif
+                    @include('corporate.comment',['items' => $comments])
+                @endforeach
+			</ol>
 
 				                <!-- START TRACKBACK & PINGBACK -->
 				                <h2 id="trackbacks">Trackbacks and pingbacks</h2>
---
+++ resources/views/corporate/comment.blade.php
@@ -0,0 +1,30 @@
+@foreach($items as $item)
+    <li id="li-comment-{{ $item->id }}" class="comment even {{ ($item->user_id == $article->user_id) ? 'bypostauthor odd' : '' }}">
+        <div id="comment-{{ $item->id }}" class="comment-container">
+            <div class="comment-author vcard">
+                @settings($hash,isset($item->email) ? md5($item->email) : md5($item->user->email))
+                <img alt="{{ $item->user->name ?? $item->name }}" src="https://gravatar.com/avatar/{{ $hash}}?mm&s=75" class="avatar" height="75" width="75">
+                <cite class="fn">{{ $item->user->name ?? $item->name }}</cite>
+            </div>
+            <div class="comment-meta commentmetadata">
+                <div class="intro">
+                    <div class="commentDate">
+                        <a href="#comment-2">{{ is_object($item->created_at) ? $item->created_at->format('F d, Y \a\t H:i') : '' }}</a>
+                    </div>
+                    <div class="commentNumber">#&nbsp;</div>
+                </div>
+                <div class="comment-body">
+                    <p>{!! $item->text !!}</p>
+                </div>
+                <div class="reply group">
+                    <a class="comment-reply-link" href="#respond" onclick="return addComment.moveForm(&quot;comment-{{ $item->id }}&quot;, &quot;{{ $item->id }}&quot;, &quot;respond&quot;, &quot;{{ $item->article_id }}&quot;)">{{ __('Reply') }}</a>
+                </div>
+            </div>
+        </div>
+        @if(isset($comment[$item->id]))
+            <ul class="children">
+                @include('corporate.comment',['items'=>$comment[$item->id]])
+            </ul>
+        @endif
+    </li>
+@endforeach
